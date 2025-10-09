5c23343e94c1599116dbc30f6f20f6f2efe04775
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2025-10-09 14:34:54 -0300

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
+
 set -e
 PASS=$(cat /run/secrets/user_database_pssw)
-export GOOSE_DBSTRING="root:${PASS}@tcp(user_database:3306)/nossochat_api?charset=utf8&parseTime=true"
+export GOOSE_DBSTRING="root:${PASS}@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true"
 exec "$@"
