cc01f64373ecc3b9b9f306134b9fbabff0e0b603
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2025-10-09 23:39:19 -0300

    cant take this anymore

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
-PASS=$(cat /run/secrets/user_database_pssw)
-export GOOSE_DBSTRING="root:${PASS}@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true"
+
+# Read and set the database string
+DB_PASSWORD=$(cat /run/secrets/user_database_pssw)
+export GOOSE_DBSTRING="root:${DB_PASSWORD}@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true"
+
+# Debug: show the first few characters of the connection string
+echo "DBSTRING: root:${DB_PASSWORD:0:3}*****@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true"
+
 exec "$@"
