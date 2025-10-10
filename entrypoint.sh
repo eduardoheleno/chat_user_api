eeaf42516fa1de4598885a093f8efdcb3a230be3
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2025-10-10 00:34:48 -0300

    please

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-# Read and set the database string
 DB_PASSWORD=$(cat /run/secrets/user_database_pssw)
 export GOOSE_DBSTRING="root:${DB_PASSWORD}@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true"
-
-# Debug: show the first few characters of the connection string
-echo "DBSTRING: root:${DB_PASSWORD:0:3}*****@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true"
 
 exec "$@"
