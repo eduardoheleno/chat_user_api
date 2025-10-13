649c05e7a67a5ee9e9dde4dae95118d7122b68b4
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2025-10-13 16:23:19 -0300

    implement database password through docker secret

--- database/database.go
+++ database/database.go
@@ -10,9 +10,13 @@ import (
 )
 
 func InitDatabase() *gorm.DB {
-	dbPssw := os.Getenv("MYSQL_ROOT_PASSWORD")
+	dbPsswPath := os.Getenv("MYSQL_ROOT_PASSWORD_FILE")
+	dbPswd, fileErr := os.ReadFile(dbPsswPath)
+	if fileErr != nil {
+		log.Panicf("Password file not found: %s", fileErr)
+	}
 
-	dsn := fmt.Sprintf("root:%s@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true", dbPssw)
+	dsn := fmt.Sprintf("root:%s@tcp(user_database:3306)/user_api?charset=utf8&parseTime=true", dbPswd)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Panicf("Failed to connect to database: %s", err)
